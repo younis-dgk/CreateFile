9e44162637ff9155bfea3a7ee0f29dd0ce189d6e
GANSTER <167035490+younis-dgk@users.noreply.github.com>
2024-10-18 14:49:00 +0500

    Update Xyz.py

--- Xyz.py
+++ Xyz.py
@@ -3,10 +3,8 @@
 # Whatsapp : +923194999455
 # More About : https://bio.link/younis_dgk
 import os, sys, platform
-print('\033[0;97m [🗂️]\033[92m Subscribe On YouTube 😍')
-os.system('xdg-open https://www.youtube.com/@YounisXyz')
 os.system('clear')
-print('\033[0;97m [📂] \033[92mChecking For Updates...')
+print('\033[0;97m [💸] \033[92mChecking For Updates...')
 os.system('git pull --quiet 2>/dev/null')
 os.system('rm -rf FILE.so')
 try:
